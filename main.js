9aa72c1f7430b9ad8d89bc874b17778dc5eeb35a
sidnei-leao <sid_devleao@hotmail.com>
2026-05-17 15:22:11 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".tenis")
+
+function mudarVisual(cor, imagem) {
+
+    body.style.background = cor
+
+    tenis.src = imagem
+}
+
+
