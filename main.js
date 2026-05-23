b4cdbf2aa43035d9097f37ecaacb3ce7b66fa70b
sidnei-leao <sid_devleao@hotmail.com>
2026-05-23 14:05:39 -0300

    Alteração-da-imagem-do-tênis-verde

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 let body = document.querySelector("body")
 let tenis = document.querySelector(".tenis")
+// let botaoCarrinho = document.querySelector(".botao-carrinho")
 
-function mudarVisual(cor, imagem) {
+function mudarVisual(cor, imagem, color) {
 
     body.style.background = cor
 
     tenis.src = imagem
+
+    // botaoCarrinho.background = color
 }
 
 
