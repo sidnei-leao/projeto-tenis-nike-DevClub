9a3e07e9fcb0bb95c736347f0c666233e531222e
sidnei-leao <sid_devleao@hotmail.com>
2026-05-28 23:19:59 -0300

    novas atualizações no JS

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-let body = document.querySelector("body")
-let tenis = document.querySelector(".tenis")
-// let botaoCarrinho = document.querySelector(".botao-carrinho")
+let body = document.querySelector("body");
+let tenis = document.querySelector(".tenis");
+let botaoCarrinho = document.querySelector(".botao-carrinho");
+let nike = document.getElementById("nike");
+let root = document.documentElement;
 
-function mudarVisual(cor, imagem, color) {
+function mudarVisual(cor, imagem) {
 
     body.style.background = cor
 
     tenis.src = imagem
 
-    // botaoCarrinho.background = color
+    botaoCarrinho.style.background = cor
+
+    nike.style.color = cor 
+
+    root.style.setProperty("--cor-neon", cor);
 }
 
 
